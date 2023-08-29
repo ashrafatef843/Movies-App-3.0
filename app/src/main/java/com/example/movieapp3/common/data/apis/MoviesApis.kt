b1792a4e5e2cf5fe55a546c730d58f3d6f2a63ac
Ashraf-atef <ashrafatef84@gmail.com>
2023-08-29 21:42:15 +0300

    Add movie details API

--- app/src/main/java/com/example/movieapp3/common/data/apis/MoviesApis.kt
+++ app/src/main/java/com/example/movieapp3/common/data/apis/MoviesApis.kt
@@ -1,15 +1,23 @@
 package com.example.movieapp3.common.data.apis
 
+import com.example.movieapp3.common.dto.MovieDetails
 import com.example.movieapp3.listing.dto.MoviesDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 const val API_KEY = "c9856d0cb57c3f14bf75bdc6c063b8f3"
 interface MoviesApis {
 
-    @GET("movie")
+    @GET("discover/movie")
     suspend fun discoverMovies(
         @Query("page") page: Int,
         @Query("api_key") apiKey: String = API_KEY
     ): MoviesDto
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetails(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = API_KEY
+    ): MovieDetails
 }
